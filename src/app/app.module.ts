8ffe7382683c8f129def462b80209c92b581d4ac
Samuel Tijani <omalitijani@gmail.com>
2020-10-18 16:57:06 +0100

    Generate customer's list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavComponent } from './navigation/sidenav/sidenav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CustomerDetailsComponent } from './customers/customer-details/customer-details.component';
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidenavComponent,
-    CustomerDetailsComponent
+    CustomerDetailsComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
