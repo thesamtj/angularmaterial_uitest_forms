321668320ece342319d15835cd19b683157bc3ad
Samuel Tijani <omalitijani@gmail.com>
2020-10-18 13:03:36 +0100

    Generate customer service

--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -16,19 +16,20 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  modified: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 1, name: 'PAUL AKPABIO', weight: 1.0079, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 2, name: 'TUNDE PHILIPS', weight: 4.0026, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 3, name: 'PETER JAMES', weight: 6.941, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 4, name: 'SOLOMON ATTAI', weight: 9.0122, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 5, name: 'MARGARET ', weight: 10.811, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 6, name: 'KIKELOMO', weight: 12.0107, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 7, name: 'ADEKUNLE', weight: 14.0067, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 8, name: 'ADISA ', weight: 15.9994, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 9, name: 'PAUL', weight: 18.9984, symbol: 'Head Office', modified: 'Samuel'},
+  { position: 10, name: 'DEBORAH', weight: 20.1797, symbol: 'Head Office', modified: 'Samuel'},
 ];
 @Component({
   selector: 'app-sidenav',
@@ -42,6 +43,7 @@ export class SidenavComponent implements OnDestroy {
     'name',
     'weight',
     'symbol',
+    'modified'
   ];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
